2589f4d98951adabae849603955e42b49b699c70
Thibo Rosemplatt <38402230+thiborose@users.noreply.github.com>
2026-06-20 19:25:49 +0200

    feat: frontend

---
+++ reonic-never-ghosted/src/ui/components/Loader/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Loader as LoaderComponent } from "./Loader";
+
+/**
+ * Add wrapper components and business logic here.
+ * If you modify this file, disable Subframe sync for it to prevent overwrites.
+ * Learn more: https://docs.subframe.com/concepts/syncing-components#wrapping-components
+ */
+
+export const Loader = LoaderComponent;
